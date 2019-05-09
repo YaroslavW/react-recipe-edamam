da002d41e79f88ce75625a470435d59a2bc01832
Yaroslav <doctor355@yahoo.com>
2019-05-09 14:42:10 +0300

    Searching on click button

--- src/App.js
+++ src/App.js
@@ -6,23 +6,40 @@ const App = () => {
 const APP_ID = "38a8ad8c";
 const APP_KEY = "e407f12584c88a55d3eed18b40a12458";
 const [recipes, setRecipes] = useState([]);
+const [search, setSearch] = useState('');
+const [query, setQuery] = useState("chicken");
 
 useEffect(() => {
   getRecipes()
-},[])
+},[query])
 
 const getRecipes = async () => {
   const response = await fetch(
-    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
   );
   const data = await response.json()
   setRecipes(data.hits)
   console.log(data.hits)
 }
+
+const updateSearch = e => {
+  setSearch(e.target.value);
+  console.log(search);
+}
+
+const getSearch = e => {
+  e.preventDefault();
+  setQuery(search);
+}
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form onSubmit={getSearch} className="search-form">
+        <input 
+        className="search-bar" 
+        value={search} 
+        type="text" 
+        onChange={updateSearch}
+        />
         <button className="search-button" type="submit">
           Search
         </button>
